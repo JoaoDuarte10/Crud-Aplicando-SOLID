67c60e69733fb6ec516473aa2418764f6c028da1
JoaoDuarte10 <joaoduarte7341@gmail.com>
2021-07-31 22:23:23 -0300

    Adicionando arquivo de conexao com o db

---
+++ src/database/Connection.ts
@@ -0,0 +1,16 @@
+require('dotenv').config();
+import { Pool } from "pg";
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: 5432
+})
+
+const sqlExecute = {
+    query: (text: string, params: any) => pool.query(text, params)
+}
+
+export { sqlExecute }
