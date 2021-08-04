8aa00ebcd8fd79b37d6867e0f8e6e70e3a373a33
JoaoDuarte10 <joaoduarte7341@gmail.com>
2021-08-04 10:16:03 -0300

    Adicionando cods de atualizaçao de um user

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import { request, response, Router } from "express";
-import { createUserController, listAllUsersController, listUserByEmailController } from "./useCases/User";
+import { createUserController, listAllUsersController, listUserByEmailController, updateUserController } from "./useCases/User";
 
 const router = Router();
 
@@ -12,6 +12,10 @@ router.get('/user/:id', (request, response)=>{
 
 router.post('/new-user', (request, response)=>{
     return createUserController.handle(request, response)
+})
+
+router.put('/update-user', (request, response)=>{
+    return updateUserController.handle(request, response)
 })
 
 export { router }
